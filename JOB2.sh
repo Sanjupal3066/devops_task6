8d0d036d4019cbb61eace3f1a95be5797a4ad4b0
Sanjupal3066 <55019137+Sanjupal3066@users.noreply.github.com>
2020-07-13 00:14:36 +0530

    job2

---
+++ JOB2.sh
@@ -0,0 +1,29 @@
+status=$(sudo cat /task6/content.txt)
+if [[ "$status" == "html" ]]
+then
+  if sudo kubectl get all | grep httpd-deploy
+	then
+		POD=$(sudo kubectl get pod -l type=http -o jsonpath="{.items[0].metadata.name}")
+		sudo kubectl cp /task6/Front.html $POD:/usr/local/apache2/htdocs/
+	else
+		sudo kubectl create -f /task6/httpd.yml
+		POD=$(sudo kubectl get pod -l type=http -o jsonpath="{.items[0].metadata.name}")
+		sleep 30
+		sudo kubectl cp /task6/Front.html $POD:/usr/local/apache2/htdocs/
+	fi
+elif [[ "$status" == "php" ]]
+then
+	if sudo kubectl get all | grep php-deploy
+	then
+		POD=$(sudo kubectl get pod -l type=php -o jsonpath="{.items[0].metadata.name}")
+		sudo kubectl cp /task6/Front.php $POD:/var/www/html/
+	else
+		sudo kubectl create -f /task6/php.yml
+		POD=$(sudo kubectl get pod -l type=php -o jsonpath="{.items[0].metadata.name}")
+		sleep 30
+		sudo kubectl cp /task6/Front.php $POD:/var/www/html
+	fi
+else
+	echo "No Server Found"
+	exit 1
+fi
